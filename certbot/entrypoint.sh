829e9b37531c5cbe3f3188b73fe5baece7024e09
IdSaf <idolarod1@gmail.com>
2020-06-22 13:15:16 +0300

    Added generation of DHE parameter and fix bad go binary build

--- certbot/entrypoint.sh
+++ certbot/entrypoint.sh
@@ -7,6 +7,7 @@ echo "Sleep interval $SLEEP_INTERVAL"
 while true
 do
 	echo "Starting certbot"
+	test -e /etc/letsencrypt/dhparam.pem || ( echo "Generate stronger DHE parameter" ; openssl dhparam -out /etc/letsencrypt/dhparam.pem 4096)
 	certbot renew --manual \
 		--manual-auth-hook '/hooks/acme_dns/acme-dns-certbot-hook -config /hooks/acme_dns/config/config.json'\
 		--preferred-challenges dns \
